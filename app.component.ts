2154d9b16038822efff9b3314f010c3c27a56230
Rohit Kumar Pachauri <rohitpachauri115@gmail.com>
2023-05-02 21:01:25 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'weather-app';
+  cityName: string = 'Boston';
+  weatherData: WeatherData;
+
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+
+        console.log(response);
+      },
+    });
+  }
+}
